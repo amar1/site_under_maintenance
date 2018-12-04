bc46251841fc2f0671132e06983c239b74bd8db1
Amrendra Mourya <amrendra.mourya@webdunia.net>
2018-12-04 19:35:39 +0530

    Fourth commit

---
+++ src/Form/SocialLinkForm.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\site_under_maintenance\Form\SocialLinkForm.
+ */
+
+namespace Drupal\site_under_maintenance\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\UrlHelper;
+
+/**
+ * Contribute SocialLinkForm form.
+ */
+class SocialLinkForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+  }
+}
+?>
